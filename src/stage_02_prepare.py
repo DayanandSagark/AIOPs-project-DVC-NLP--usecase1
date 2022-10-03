d23f360a209efe6785a9b0c04372a3f0f37f9773
dayanand <dayanand_87@yahoo.co.in>
2022-10-03 23:36:19 +0530

    stage-2 script changes data_mgmt and prepare changes

--- src/stage_02_prepare.py
+++ src/stage_02_prepare.py
@@ -1,10 +1,11 @@
 import argparse
 import os
+from re import L
 import shutil
-from tqdm import tqdm
 import logging
 from src.utils.common import read_yaml, create_directories
 import random
+from src.utils.data_mgmt import process_posts
 
 
 STAGE = "stage 02 prepare data" ## <<< change stage name 
@@ -21,7 +22,32 @@ def main(config_path, params_path):
     ## read config files
     config = read_yaml(config_path)
     params = read_yaml(params_path)
-    pass
+    ## Load data file
+    local_data_dir = config["source_download_dir"]["data_dir"]
+ 
+    data_filename = config["source_download_dir"]["data_file"]
+    input_data = os.path.join(local_data_dir,data_filename)
+    # Load params split and seed
+    split = params["prepare"]["split"]
+    seed = params["prepare"]["seed"]
+    random.seed(seed)
+
+    # Load artifacts directory
+    artifacts = config["artifacts"]
+    prepared_data_dir_path = os.path.join(artifacts["ARTIFACTS_DIR"],artifacts["PREPARED_DATA"])
+    create_directories([prepared_data_dir_path])
+    # Train and Test data paths
+    train_data_path = os.path.join(prepared_data_dir_path,artifacts["TRAIN_DATA"])
+    test_data_path = os.path.join(prepared_data_dir_path,artifacts["TEST_DATA"])
+    
+    encode = "utf8"
+    with open(input_data, encoding="utf8") as f_in:
+        with open(train_data_path,"w",encoding = encode) as f_out_train:
+            with open(test_data_path,"w",encoding = encode) as f_out_test:
+                process_posts(f_in,f_out_train,f_out_test,"<python>",split)
+
+
+
 
 
 if __name__ == '__main__':
