18702d5c5756ce561f5f255dac81584714e29036
dayanand <dayanand_87@yahoo.co.in>
2022-10-04 12:57:01 +0530

    Stage 04 py script train stage changes completed

--- src/utils/featurize.py
+++ src/utils/featurize.py
@@ -1,21 +1,17 @@
-import os
+
 import logging
-import pandas as pd
 import joblib
 import scipy.sparse as sparse
 import numpy as np
 
-
-
-def save_matrix(df,matrix,out_path):
+def save_matrix(df, matrix, out_path):
     id_matrix = sparse.csr_matrix(df.id.astype(np.int64)).T
     label_matrix = sparse.csr_matrix(df.label.astype(np.int64)).T
 
-    result = sparse.hstack([id_matrix,label_matrix], format = "csr")
-    
-    joblib.dump(result,out_path)
-    msg = f"the output matrix saved at: {out_path} of size: {result.shape} and datatype: {result.dtype}"
-    logging.info(msg)
+    result = sparse.hstack([id_matrix, label_matrix, matrix], format="csr")
 
+    joblib.dump(result, out_path)
+    msg = f"The output matrix saved at: {out_path} of the size: {result.shape} and data type: {result.dtype}"
+    logging.info(msg)
 
 
